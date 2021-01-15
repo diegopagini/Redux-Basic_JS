4f75b53808a92800dbed847048091bf5c8d24ba0
Diego Pagini <dq@qbkconsulting.com>
2021-01-15 15:35:46 -0300

    mas acciones agregadas al reducer

--- app-1.ts
+++ app-1.ts
@@ -12,10 +12,25 @@ interface Action {
 
 // Un reducer es una funcion que recibe siempre, un estado y una acccion. O State y Action.
 function reducer(state = 10, action: Action) {
-	if (action.type === 'INCREMENTAR') {
-		return (state += 1);
+	// if (action.type === 'INCREMENTAR') {
+	// 	return (state += 1);
+	// }
+	switch (action.type) {
+		case 'INCREMENTAR':
+			return (state += 1);
+
+		case 'DECREMENTAR':
+			return (state -= 1);
+
+		case 'MULTIPLICAR':
+			return state * action.payload;
+
+		case 'DIVIDIR':
+			return state / action.payload;
+
+		default:
+			return state;
 	}
-	return state;
 }
 
 // Usar el Reducer
@@ -24,5 +39,21 @@ function reducer(state = 10, action: Action) {
 const incrementadorAction: Action = {
 	type: 'INCREMENTAR',
 };
+console.log(reducer(10, incrementadorAction)); //11
 
-console.log(reducer(10, incrementadorAction));
+const decrementadorAction: Action = {
+	type: 'DECREMENTAR',
+};
+console.log(reducer(10, decrementadorAction)); //9
+
+const multiplicarAction: Action = {
+	type: 'MULTIPLICAR',
+	payload: 2,
+};
+console.log(reducer(10, multiplicarAction)); //20
+
+const dividirAction: Action = {
+	type: 'DIVIDIR',
+	payload: 2,
+};
+console.log(reducer(10, dividirAction)); //5
